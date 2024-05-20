2ee91f85232e6d6185630fd4908387ab033a16a2
Egor Ryzhov <32880315+Ryjov@users.noreply.github.com>
2024-05-20 12:39:57 +0300

    Добавьте файлы проекта.

---
+++ EmailSender/EmailService.cs
@@ -0,0 +1,31 @@
+﻿using MimeKit;
+using MailKit.Net.Smtp;
+
+namespace EmailSender
+{
+    public class EmailService
+    {
+        public async Task SendTextMail(string to, string text)
+        {
+            using (var message = new MimeMessage())
+            {
+                message.From.Add(new MailboxAddress("ERSender", "bolt321@mail.ru"));
+                message.To.Add(new MailboxAddress("", to));
+                message.Subject = "Testing";
+                message.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+                {
+                    Text = text
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    await client.ConnectAsync("smtp.yandex.ru", 25, false);
+                    await client.AuthenticateAsync("login@yandex.ru", "password");
+                    await client.SendAsync(message);
+
+                    await client.DisconnectAsync(true);
+                }
+            }
+        }
+    }
+}
---
+++ EmailSender/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailSender
+{
+    public class Receiver
+    {
+        public async Task ReceiveMessage()
+        {
+            ConnectionFactory factory = new();
+            factory.Uri = new Uri(uriString: );
+            factory.ClientProvidedName = "Rabbit sender app";
+
+            IConnection cnn = factory.CreateConnection();
+            IModel channel = cnn.CreateModel();
+
+            string exchangeName = "DemoExchange";
+            string routingKey = "demo-routing-key";
+            string queueName = "DemoQueue";
+        }
+    }
+}
